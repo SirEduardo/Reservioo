2588aae738b91c058eb7ffb33c8491d39e0a853e
Eduard Sanchez <eduardosaanchezlopez@gmail.com>
2025-06-23 11:18:25 +0200

    mockUpdate

--- frontend/src/app/mocks.ts
+++ frontend/src/app/mocks.ts
@@ -1,3 +1,6 @@
+import { Calendar, Users, Briefcase, AlarmClock, Settings } from 'lucide-react'
+
+
 export const DAYS_OF_WEEK = [
     { value: 0, label: "Domingo" },
     { value: 1, label: "Lunes" },
@@ -8,4 +11,10 @@ export const DAYS_OF_WEEK = [
     { value: 6, label: "Sábado" },
   ]
   
-  
+  export const tabs = [
+    { id: 'reservas', label: 'Reservas', icon: Calendar },
+    { id: 'professionals', label: 'Profesionales', icon: Users },
+    { id: 'services', label: 'Servicios', icon: Briefcase },
+    { id: 'schedules', label: 'Horarios', icon: AlarmClock },
+    { id: 'settings', label: 'Configuración', icon: Settings }
+  ]
